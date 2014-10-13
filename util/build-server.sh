d4fb897258ec9ff3d5b7410e2a846f1b89ef39ba
donat_b <donat@opmbx.org>
2014-10-13 11:32:28 +0400

    Update build scripts

--- util/build-server.sh
+++ util/build-server.sh
@@ -3,31 +3,18 @@ set -e
 
 libdir=~/git/lib
 
-# Get minetest
-git_hash=`git show | head -c14 | tail -c7`
-
-[[ ! -e $libdir ]] && mkdir $libdir
-if [[ ! -d $libdir/luajit ]]; then
-	git clone http://luajit.org/git/luajit-2.0.git $libdir/luajit
-fi
-if [[ ! -d $libdir/leveldb ]]; then
-	git clone https://github.com/google/leveldb $libdir/leveldb
-fi
-
 cmake . \
 	-DRUN_IN_PLACE=1 \
-	-DCMAKE_CXX_FLAGS='-O3 -g' -DCMAKE_C_FLAGS='-O3' \
 	-DBUILD_CLIENT=0 -DBUILD_SERVER=1 \
-	\
-	-DENABLE_SOUND=0 \
 	-DENABLE_CURL=1 \
+	-DENABLE_LEVELDB=1 \
 	-DENABLE_GETTEXT=0 \
 	-DENABLE_FREETYPE=0 \
-	-DENABLE_LEVELDB=1 \
-	\
+	-DENABLE_SOUND=0 \
 	-DLEVELDB_INCLUDE_DIR=$libdir/leveldb/include \
-	-DLEVELDB_LIBRARY=$libdir/leveldb/libleveldb.so
+	-DLEVELDB_LIBRARY=$libdir/leveldb/libleveldb.so \
+	-DCMAKE_BUILD_TYPE=Debug
+	#-DCMAKE_CXX_FLAGS='-O3 -g' -DCMAKE_C_FLAGS='-O3' \
 
-make -j2
+make -j3
 
-# EOF
