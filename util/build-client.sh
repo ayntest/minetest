0fa68e68c31d6cdb00e1e3c1737fb44f2b3c9bf4
donat_b <donat@opmbx.org>
2014-10-15 22:13:50 +0400

    Update build script

--- util/build-client.sh
+++ util/build-client.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-	#-DCMAKE_CXX_FLAGS='-O3 -g' \
-git_hash=$(git show | head -c14 | tail -c7)
 cmake . \
+	-DCMAKE_CXX_FLAGS='-O3' \
 	-DRUN_IN_PLACE=0 \
 	-DBUILD_SERVER=0 \
 	-DBUILD_CLIENT=1 \
@@ -11,12 +10,11 @@ cmake . \
 	-DENABLE_CURL=1 \
 	-DENABLE_GETTEXT=0 \
 	-DENABLE_SOUND=1 \
-	-DENABLE_LEVELDB=0 \
+	-DENABLE_LEVELDB=1 \
 	-DENABLE_REDIS=0 \
 	-DIRRLICHT_SOURCE_DIR=~/git/irrlicht/sources \
 	-DIRRLICHT_INCLUDE_DIR=~/git/irrlicht/include \
 	-DIRRLICHT_LIBRARY=~/git/irrlicht/lib/Linux/libIrrlicht.a \
-	-DCMAKE_BUILD_TYPE=Release
 
 /usr/bin/time make -j3
 sudo checkinstall -y \
